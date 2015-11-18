4124df208cdd49d11034bec9c2949498c1439424
authorbeard <msnolan@gmail.com>
2015-11-18 16:17:51 -0500

    all tests passing & all samples cleared

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,7 @@
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
+  # puts status
 end
 
 
@@ -8,7 +9,7 @@ end
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
+rescue_princess_peach
 # puts status
 
 
