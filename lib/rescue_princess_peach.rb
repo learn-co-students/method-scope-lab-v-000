22eb7ea95af4d52035f40a79e269efe5dbef047b
charlie lee <ctlee9096@gmail.com>
2016-12-01 02:38:34 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -2,8 +2,8 @@ def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
-rescue_princess_peach
-puts status
+#rescue_princess_peach
+#puts status
 
 
 
