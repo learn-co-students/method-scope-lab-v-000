5e6d8b5ffe328ed8997f0f5a5937b23e55e19856
Joanna Sese <jmgsese@gmail.com>
2017-07-13 17:37:41 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,7 @@
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
+
 end
 
 
@@ -10,7 +11,3 @@ end
 
 # rescue_princess_peach
 # puts status
-
-
-
-
