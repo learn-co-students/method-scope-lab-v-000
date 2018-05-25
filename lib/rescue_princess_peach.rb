ccd404d75faa8074ad38542607e3106c299c14aa
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-25 04:32:39 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -4,13 +4,9 @@ def rescue_princess_peach
 end
 
 
-
+#status = 'hi'
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
-# puts status
-
-
-
-
+rescue_princess_peach #runs the rescue princess peach method that is defined on line 1
+#puts status
