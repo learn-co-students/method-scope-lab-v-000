3a5eb9d6d6923f6663b39921d38c216e37d4c83b
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-17 08:20:49 +0000

    Done.

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,8 +1,9 @@
 require "spec_helper"
+require_relative "../lib/catch_phrase.rb"
 
-describe "#catch_phrase" do 
-  it "puts out a catch phrase" do 
-    phrase = "It's-a me, Mario!"
-    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
+describe "#catch_phrase" do
+  it "puts out a catch phrase" do
+    phrase = "It's-a me, Mario"
+    expect{catch_phrase(phrase)}.to output("It's-a me, Mario\n").to_stdout
   end
 end
