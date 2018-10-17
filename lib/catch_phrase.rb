78cb0a237256e1c10597cf32bf57b21063e96361
Lois Lee <lois.lee@me.com>
2018-10-17 05:31:13 +0000

    Done.

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,4 +1,4 @@
-
-def catch_phrase (phrase)
+def catch_phrase (phrase = "It's-a me, Mario!")
   puts phrase
+
 end
--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,7 @@
-def rescue_princess_peach 
+
+def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puts status
+rescue_princess_peach
